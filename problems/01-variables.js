4a65687bad6e4483103ccf19a072944902214de7
jenW79 <jenwilliams79123@gmail.com>
2024-06-08 14:14:46 -0500

    uploading new files

--- problems/01-variables.js
+++ problems/01-variables.js
@@ -5,36 +5,35 @@ test your work by running your code and the test specs!
 
 // 1. Declare a variable called `firstName` and assign it your first name
 //    as a string:
-
-// Your code here 
+let firstName = 'Jennifer';
 
 
 // 2. Declare a variable called `lastName` and assign it your last name
 //    as a string:
 
-// Your code here 
+let lastName = 'Williams'; 
 
 
 // 3. Declare a variable called `age`:
 
-// Your code here 
+let age; 
 
 
 // 4. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
-// Your code here 
+console.log(firstName + " " + lastName + " " + age);
 
 
 // 5. Assign the `age` variable to a number:
 
-// Your code here 
+age = '44';
 
 
 // 6. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
-// Your code here 
+console.log(firstName + " " + lastName + " " + age);
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
